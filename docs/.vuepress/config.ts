d3f3be844da0f49a3801d38394788a51813e0757
Mo Alsharbaji <msharbaji@gmail.com>
2023-07-02 23:29:57 +0200

    add base repo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/msharbaji.github.io/",
 
   lang: "en-US",
   title: "DevJourney - With Mo Alsharbaji",
